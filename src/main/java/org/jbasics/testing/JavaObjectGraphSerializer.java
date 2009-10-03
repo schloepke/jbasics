5cb53321ca1c590cb03121de7ef0229cad8704d5
Stephan Schloepke <stephan@schloepke.de>
2009-10-04 00:37:48 +0200

    Added javadoc to JavaObjectGraphSerializer

--- src/main/java/org/jbasics/testing/JavaObjectGraphSerializer.java
+++ src/main/java/org/jbasics/testing/JavaObjectGraphSerializer.java
@@ -33,15 +33,46 @@ import java.io.InputStream;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.io.OutputStream;
+import java.io.Serializable;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
 import org.jbasics.checker.ContractCheck;
 
+/**
+ * Simple Helper to serialize java object graphs which are {@link Serializable}.
+ * <p>
+ * In order to use the helper you need to instantiate it. The helper itself is fully thread safe and
+ * can easily be used as a singleton. However the logging used needs to be thread safe as well since
+ * the helper does use a private attribute of the logger instance. Since Java Logging is used there
+ * should be no problem involved.
+ * </p>
+ * <p>
+ * The easiest way to use the helper is to create one use it and destroy it right away (unless you
+ * serialize more than one graph at a time).
+ * </p>
+ * <p>
+ * Example:<br />
+ * {@code new JavaObjectGraphSerializer().serialize(instance, new File(filename))}
+ * </p>
+ * 
+ * @author Stephan Schloepke
+ * @since 1.0
+ */
 public final class JavaObjectGraphSerializer {
 	private static final Level LOG_LEVEL = Level.FINE;
 	private final Logger logger = Logger.getLogger(JavaObjectGraphSerializer.class.getName());
 
+	/**
+	 * Serialize the given object graph to the file name in the directory. If any of the supplied
+	 * arguments is null no serialization takes place and false is returned. If serialization
+	 * completed successfully true is returned.
+	 * 
+	 * @param objectGraph The object graph
+	 * @param directory The directory to serialize the file to
+	 * @param fileName The filename to create in the directory
+	 * @return True if the serialization was successful otherwise false
+	 */
 	public boolean serialize(Object objectGraph, File directory, String fileName) {
 		if (objectGraph == null || directory == null || fileName == null) {
 			if (this.logger.isLoggable(LOG_LEVEL)) {
