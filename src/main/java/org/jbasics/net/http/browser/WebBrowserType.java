09532957b362ad3d030e4e8c9f068ef2d20b9c0a
Stephan Schloepke <stephan@schloepke.de>
2010-02-18 19:32:53 +0100

    Adding WebBrowser detection for servlet and other http usage

---
+++ src/main/java/org/jbasics/net/http/browser/WebBrowserType.java
@@ -0,0 +1,54 @@
+/*
+ * Copyright (c) 2009 Stephan Schloepke and innoQ Deutschland GmbH
+ *
+ * Stephan Schloepke: http://www.schloepke.de/
+ * innoQ Deutschland GmbH: http://www.innoq.com/
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package org.jbasics.net.http.browser;
+
+import java.util.regex.Pattern;
+
+public enum WebBrowserType {
+	SAFARI("Safari/[0-9]*"), // Safari Browser
+	FIREFOX("Firefox/[0-9]\\.[0-9]{1,2}"), // Firefox Browser
+	INTERNET_EXPLORER("MSIE [0-9]\\.[0-9]{1,2}"), // Internet Explorer Browser
+	OPERA("Opera(/| )[0-9]\\.[0-9]{1,2}"), // Opera Browser
+	KONQUEROR("Konqueror/.*;"), // Konqueror Browser
+	OMNIWEB("OmniWeb/[0-9]\\.[0-9]{1,2}"), // OmniWeb Browser (on Apple)
+	MOZILLA("Mozilla/[0-9]\\.[0-9]{1,2}"), // Mozilla Browser (Matches Mozilla compat also)
+	OTHER(".*"); // Unknown browser not detected
+
+	public Pattern detectPattern;
+
+	WebBrowserType(String detectPattern) {
+		this.detectPattern = Pattern.compile(detectPattern);
+	}
+
+	public static WebBrowserType detect(String userAgent) {
+		for (WebBrowserType temp : values()) {
+			if (temp.detectPattern.matcher(userAgent).find()) {
+				return temp;
+			}
+		}
+		return OTHER;
+	}
+
+}
