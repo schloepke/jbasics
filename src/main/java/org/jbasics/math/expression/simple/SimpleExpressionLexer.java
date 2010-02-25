9403826f85ed797021fbbf05bae2b5ce4199d486
Stephan Schloepke <stephan@schloepke.de>
2010-02-25 15:46:35 +0100

    Adding simple expression system

---
+++ src/main/java/org/jbasics/math/expression/simple/SimpleExpressionLexer.java
@@ -0,0 +1,147 @@
+/*
+ * Copyright (c) 2009 Stephan Schloepke and innoQ Deutschland GmbH
+ *
+ * Stephan Schloepke: http://www.schloepke.de/
+ * innoQ Deutschland GmbH: http://www.innoq.com/
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package org.jbasics.math.expression.simple;
+
+import org.jbasics.checker.ContractCheck;
+
+public class SimpleExpressionLexer {
+
+    public static enum TokenType {
+        ADD, SUBTRACT, MULTIPLY, DIVIDE, LEFT_BRACE, RIGHT_BRACE, NUMBER, SYMBOL, EOF
+    }
+
+    private final CharSequence sequence;
+    private final StringBuilder currentContent;
+    private int position;
+    private TokenType currentType;
+
+    public SimpleExpressionLexer(CharSequence sequence) {
+        this.sequence = ContractCheck.mustNotBeNull(sequence, "sequence");
+        this.currentContent = new StringBuilder(32);
+        this.position = 0;
+    }
+
+    public TokenType curentType() {
+        return this.currentType;
+    }
+
+    public StringBuilder currentContent() {
+        return this.currentContent;
+    }
+
+    public boolean isExpectedType(TokenType... types) {
+        for (TokenType type : types) {
+            if (type == this.currentType) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public SimpleExpressionLexer next() {
+        this.currentContent.setLength(0);
+        if (this.position < this.sequence.length()) {
+            char c = this.sequence.charAt(this.position++);
+            while (Character.isWhitespace(c)) {
+                c = this.sequence.charAt(this.position++);
+            }
+            switch (c) {
+            case '+':
+                this.currentContent.append("+");
+                this.currentType = TokenType.ADD;
+                break;
+            case '-':
+                this.currentContent.append("-");
+                this.currentType = TokenType.SUBTRACT;
+                break;
+            case '*':
+                this.currentContent.append("*");
+                this.currentType = TokenType.MULTIPLY;
+                break;
+            case '/':
+                this.currentContent.append("/");
+                this.currentType = TokenType.DIVIDE;
+                break;
+            case '(':
+                this.currentContent.append("(");
+                this.currentType = TokenType.LEFT_BRACE;
+                break;
+            case ')':
+                this.currentContent.append(")");
+                this.currentType = TokenType.RIGHT_BRACE;
+                break;
+            default:
+                parseNumberOrIdentifier(c);
+            }
+        } else {
+            this.currentType = TokenType.EOF;
+        }
+        return this;
+    }
+
+    private void parseNumberOrIdentifier(char c) {
+        if (Character.isDigit(c) || c == '.') {
+        	boolean dotFound = (c == '.');
+            this.currentType = TokenType.NUMBER;
+            this.currentContent.append(c);
+            while (this.position < this.sequence.length()) {
+                char t = this.sequence.charAt(this.position);
+                if (Character.isDigit(t) || t == '.') {
+                	if (t == '.') {
+                		if (dotFound) {
+                			throw new RuntimeException("Illegal number. Number cannot have multiple decimal seperators " + this.currentContent);
+                		} else {
+                			dotFound = true;
+                		}
+                	}
+                    this.currentContent.append(t);
+                    this.position++;
+                } else {
+                    break;
+                }
+            }
+        } else if (Character.isJavaIdentifierStart(c)) {
+            this.currentType = TokenType.SYMBOL;
+            if (c != '$') {
+                this.currentContent.append(c);
+            }
+            while (this.position < this.sequence.length()) {
+                char t = this.sequence.charAt(this.position);
+                if (Character.isJavaIdentifierPart(t)) {
+                    this.currentContent.append(t);
+                    this.position++;
+                } else {
+                    break;
+                }
+            }
+            if (this.currentContent.length() == 0) {
+                throw new RuntimeException("ParseError: Empty identifier found");
+            }
+        } else {
+            throw new RuntimeException("ParseError: Unknown token start found " + c);
+        }
+    }
+
+}
---
+++ src/test/java/org/jbasics/math/expression/simple/SimpleExpressionLexerTest.java
@@ -0,0 +1,67 @@
+/*
+ * Copyright (c) 2009 Stephan Schloepke and innoQ Deutschland GmbH
+ *
+ * Stephan Schloepke: http://www.schloepke.de/
+ * innoQ Deutschland GmbH: http://www.innoq.com/
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package org.jbasics.math.expression.simple;
+
+import java.util.Arrays;
+import java.util.Collection;
+
+import org.jbasics.math.expression.simple.SimpleExpressionLexer;
+import org.jbasics.testing.Java14LoggingTestCase;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
+
+@RunWith(Parameterized.class)
+public class SimpleExpressionLexerTest extends Java14LoggingTestCase {
+
+	@Parameters
+	public static Collection<String[]> data() {
+		return Arrays.asList(
+				new String[] { "($4020_15 - $7012_c) + ($5020_v - $5120_c) * 100" },
+				new String[] { "42 * 7 + HalloWelt" },
+				new String[] { "$4010_v" },
+				new String[] { "$2060_c+$100_a-$900_b" }
+		);
+	}
+
+	private final String testExpression;
+
+	public SimpleExpressionLexerTest(String testExpression) {
+		this.testExpression = testExpression;
+	}
+
+	@Test
+	public void test() {
+		this.logger.info("------------");
+		this.logger.info("Expression: " + testExpression);
+		SimpleExpressionLexer temp = new SimpleExpressionLexer(testExpression);
+		while (temp.next().curentType() != SimpleExpressionLexer.TokenType.EOF) {
+			this.logger.info("Found: " + temp.curentType() + " {" + temp.currentContent() + "}");
+		}
+		this.logger.info("Found: " + temp.curentType() + " {" + temp.currentContent() + "}");
+	}
+
+}
