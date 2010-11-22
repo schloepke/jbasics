252e7b5fb35c79cd9519e0c3a99b0e8031bdff36
Stephan Schloepke <stephan@schloepke.de>
2010-11-22 19:26:13 +0100

    Updated parsing of date instances since the parser was already there but the usage was missing

--- src/main/java/org/jbasics/jee/servlet/HttpServletRequestAccessor.java
+++ src/main/java/org/jbasics/jee/servlet/HttpServletRequestAccessor.java
@@ -34,6 +34,7 @@ import javax.servlet.http.HttpServletRequest;
 
 import org.jbasics.arrays.ArrayConstants;
 import org.jbasics.checker.ContractCheck;
+import org.jbasics.exception.DelegatedException;
 
 /**
  * This accessor can be used to access parameter values in the request in a more convinient way.
@@ -46,14 +47,14 @@ public class HttpServletRequestAccessor {
 	private final HttpServletRequest request;
 
 	/**
-	 * Create an accessor for the given {@link HttpServletRequest}. An
-	 * {@link IllegalArgumentException} is thrown if the request is null.
+	 * Create an accessor for the given {@link HttpServletRequest}. An {@link IllegalArgumentException} is thrown if the
+	 * request is null.
 	 * 
 	 * @param request The request to access the parameters from (must not be null)
 	 * @throws IllegalArgumentException If the request is null
 	 */
-	public HttpServletRequestAccessor(HttpServletRequest request) {
-		this.request = ContractCheck.mustNotBeNull(request, "request");
+	public HttpServletRequestAccessor(final HttpServletRequest request) {
+		this.request = ContractCheck.mustNotBeNull(request, "request"); //$NON-NLS-1$
 	}
 
 	/**
@@ -65,22 +66,22 @@ public class HttpServletRequestAccessor {
 		return this.request;
 	}
 
-	public String getStringParameter(String name, String defaultValue) {
-		String temp = this.request.getParameter(ContractCheck.mustNotBeNull(name, "name"));
+	public String getStringParameter(final String name, final String defaultValue) {
+		String temp = this.request.getParameter(ContractCheck.mustNotBeNull(name, "name")); //$NON-NLS-1$
 		return temp != null ? temp : defaultValue;
 	}
 
-	public String[] getStringParameters(String name) {
-		return this.request.getParameterValues(ContractCheck.mustNotBeNull(name, "name"));
+	public String[] getStringParameters(final String name) {
+		return this.request.getParameterValues(ContractCheck.mustNotBeNull(name, "name")); //$NON-NLS-1$
 	}
 
-	public Integer getIntegerParameter(String name, Integer defaultValue) {
-		String temp = this.request.getParameter(ContractCheck.mustNotBeNull(name, "name"));
+	public Integer getIntegerParameter(final String name, final Integer defaultValue) {
+		String temp = this.request.getParameter(ContractCheck.mustNotBeNull(name, "name")); //$NON-NLS-1$
 		return temp != null ? Integer.valueOf(temp) : defaultValue;
 	}
 
-	public Integer[] getIntegerParameters(String name) {
-		String[] temp = this.request.getParameterValues(ContractCheck.mustNotBeNull(name, "name"));
+	public Integer[] getIntegerParameters(final String name) {
+		String[] temp = this.request.getParameterValues(ContractCheck.mustNotBeNull(name, "name")); //$NON-NLS-1$
 		if (temp == null || temp.length == 0) {
 			return ArrayConstants.ZERO_LENGTH_INTEGER_TYPE_ARRAY;
 		}
@@ -91,13 +92,13 @@ public class HttpServletRequestAccessor {
 		return result;
 	}
 
-	public Long getLongParameter(String name, Long defaultValue) {
-		String temp = this.request.getParameter(ContractCheck.mustNotBeNull(name, "name"));
+	public Long getLongParameter(final String name, final Long defaultValue) {
+		String temp = this.request.getParameter(ContractCheck.mustNotBeNull(name, "name")); //$NON-NLS-1$
 		return temp != null ? Long.valueOf(temp) : defaultValue;
 	}
 
-	public Long[] getLongParameters(String name) {
-		String[] temp = this.request.getParameterValues(ContractCheck.mustNotBeNull(name, "name"));
+	public Long[] getLongParameters(final String name) {
+		String[] temp = this.request.getParameterValues(ContractCheck.mustNotBeNull(name, "name")); //$NON-NLS-1$
 		if (temp == null || temp.length == 0) {
 			return ArrayConstants.ZERO_LENGTH_LONG_TYPE_ARRAY;
 		}
@@ -108,13 +109,13 @@ public class HttpServletRequestAccessor {
 		return result;
 	}
 
-	public Number getNumberParameter(String name, Number defaultValue) {
-		String temp = this.request.getParameter(ContractCheck.mustNotBeNull(name, "name"));
+	public Number getNumberParameter(final String name, final Number defaultValue) {
+		String temp = this.request.getParameter(ContractCheck.mustNotBeNull(name, "name")); //$NON-NLS-1$
 		return temp != null ? parseNumber(temp) : defaultValue;
 	}
 
-	public Number[] getNumberParameters(String name) {
-		String[] temp = this.request.getParameterValues(ContractCheck.mustNotBeNull(name, "name"));
+	public Number[] getNumberParameters(final String name) {
+		String[] temp = this.request.getParameterValues(ContractCheck.mustNotBeNull(name, "name")); //$NON-NLS-1$
 		if (temp == null || temp.length == 0) {
 			return ArrayConstants.ZERO_LENGTH_NUMBER_ARRAY;
 		}
@@ -125,13 +126,13 @@ public class HttpServletRequestAccessor {
 		return result;
 	}
 
-	public URI getURIParameter(String name, URI defaultValue) {
-		String temp = this.request.getParameter(ContractCheck.mustNotBeNull(name, "name"));
+	public URI getURIParameter(final String name, final URI defaultValue) {
+		String temp = this.request.getParameter(ContractCheck.mustNotBeNull(name, "name")); //$NON-NLS-1$
 		return temp != null ? URI.create(temp) : defaultValue;
 	}
 
-	public URI[] getURIParameters(String name) {
-		String[] temp = this.request.getParameterValues(ContractCheck.mustNotBeNull(name, "name"));
+	public URI[] getURIParameters(final String name) {
+		String[] temp = this.request.getParameterValues(ContractCheck.mustNotBeNull(name, "name")); //$NON-NLS-1$
 		if (temp == null || temp.length == 0) {
 			return ArrayConstants.ZERO_LENGTH_URI_ARRAY;
 		}
@@ -142,13 +143,13 @@ public class HttpServletRequestAccessor {
 		return result;
 	}
 
-	public Boolean getBooleanParameter(String name, Boolean defaultValue) {
-		String temp = this.request.getParameter(ContractCheck.mustNotBeNull(name, "name"));
+	public Boolean getBooleanParameter(final String name, final Boolean defaultValue) {
+		String temp = this.request.getParameter(ContractCheck.mustNotBeNull(name, "name")); //$NON-NLS-1$
 		return temp != null ? Boolean.valueOf(temp) : defaultValue;
 	}
 
-	public Boolean[] getBooleanParameters(String name) {
-		String[] temp = this.request.getParameterValues(ContractCheck.mustNotBeNull(name, "name"));
+	public Boolean[] getBooleanParameters(final String name) {
+		String[] temp = this.request.getParameterValues(ContractCheck.mustNotBeNull(name, "name")); //$NON-NLS-1$
 		if (temp == null || temp.length == 0) {
 			return ArrayConstants.ZERO_LENGTH_BOOLEAN_TYPE_ARRAY;
 		}
@@ -159,13 +160,13 @@ public class HttpServletRequestAccessor {
 		return result;
 	}
 
-	public Date getDateParameter(String name, Date defaultValue) {
-		String temp = this.request.getParameter(ContractCheck.mustNotBeNull(name, "name"));
+	public Date getDateParameter(final String name, final Date defaultValue) {
+		String temp = this.request.getParameter(ContractCheck.mustNotBeNull(name, "name")); //$NON-NLS-1$
 		return temp != null ? parseDate(temp) : defaultValue;
 	}
 
-	public Date[] getDateParameters(String name) {
-		String[] temp = this.request.getParameterValues(ContractCheck.mustNotBeNull(name, "name"));
+	public Date[] getDateParameters(final String name) {
+		String[] temp = this.request.getParameterValues(ContractCheck.mustNotBeNull(name, "name")); //$NON-NLS-1$
 		if (temp == null || temp.length == 0) {
 			return ArrayConstants.ZERO_LENGTH_DATE_ARRAY;
 		}
@@ -176,27 +177,30 @@ public class HttpServletRequestAccessor {
 		return result;
 	}
 
-	private Date parseDate(String stringValue) {
+	private Date parseDate(final String stringValue) {
 		assert stringValue != null;
-		DateFormat f1 = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSZ");
-		DateFormat f2 = new SimpleDateFormat("yyMMddHHmmssZ");
+		DateFormat f1 = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSZ"); //$NON-NLS-1$
+		DateFormat f2 = new SimpleDateFormat("yyMMddHHmmssZ"); //$NON-NLS-1$
 		try {
 			return f1.parse(stringValue);
 		} catch (ParseException e) {
-			// TODO here we want more
-			throw new RuntimeException(e);
+			try {
+				return f2.parse(stringValue);
+			} catch (ParseException ee) {
+				throw DelegatedException.delegate(ee);
+			}
 		}
 	}
 
 	/**
-	 * Parse the given {@link String} to a {@link Number}. The parser first parses the
-	 * {@link String} to a {@link Double} and than checks if the value can fit in a lesser type
+	 * Parse the given {@link String} to a {@link Number}. The parser first parses the {@link String} to a
+	 * {@link Double} and than checks if the value can fit in a lesser type
 	 * until {@link Integer} is reached.
 	 * 
 	 * @param stringValue The value to parse (must not be null)
 	 * @return The parsed number ({@link Double}, {@link Float}, {@link Long} or {@link Integer})
 	 */
-	private Number parseNumber(String stringValue) {
+	private Number parseNumber(final String stringValue) {
 		assert stringValue != null;
 		Double val = Double.valueOf(stringValue);
 		double x = val.doubleValue();
