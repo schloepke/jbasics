b28830bc4fc6b92d347bbfa356efb516364b1b84
Stephan Schloepke <stephan@schloepke.de>
2012-09-24 21:11:47 +0200

    Renamed ContextualResolveStrategyDiscoveryFactory to ContextualResolveStrategiesDiscoverFactory to reflect in the name that multiple instances are discovered.

--- src/main/java/org/jbasics/types/strategy/ContextualResolveStrategiesDiscoverFactory.java
+++ src/main/java/org/jbasics/types/strategy/ContextualResolveStrategiesDiscoverFactory.java
@@ -1,19 +1,19 @@
 /*
  * Copyright (c) 2009 Stephan Schloepke and innoQ Deutschland GmbH
- *
+ * 
  * Stephan Schloepke: http://www.schloepke.de/
  * innoQ Deutschland GmbH: http://www.innoq.com/
- *
+ * 
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
  * in the Software without restriction, including without limitation the rights
  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  * copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
- *
+ * 
  * The above copyright notice and this permission notice shall be included in
  * all copies or substantial portions of the Software.
- *
+ * 
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -24,24 +24,15 @@
  */
 package org.jbasics.types.strategy;
 
-import java.util.Map;
-
 import org.jbasics.checker.ContractCheck;
-import org.jbasics.discover.ServiceClassDiscovery;
-import org.jbasics.pattern.factory.Factory;
+import org.jbasics.discover.GenericsMappedInstanceDiscoveryFactory;
 import org.jbasics.pattern.strategy.ContextualResolveStrategy;
-import org.jbasics.types.sequences.Sequence;
 
-public class ContextualResolveStrategyDiscoverFactory<Context> implements
-		Factory<Map<Sequence<Class<?>>, ContextualResolveStrategy<?, ?, Context>>> {
-	private final Class<? extends Context> contextType;
+public class ContextualResolveStrategiesDiscoverFactory<Context> extends
+		GenericsMappedInstanceDiscoveryFactory<ContextualResolveStrategy<?, ?, Context>> {
 
-	public ContextualResolveStrategyDiscoverFactory(final Class<? extends Context> contextType) {
-		this.contextType = ContractCheck.mustNotBeNull(contextType, "contextType"); //$NON-NLS-1$
+	public ContextualResolveStrategiesDiscoverFactory(final Class<? extends Context> contextType) {
+		super(ContextualResolveStrategy.class, null, null, ContractCheck.mustNotBeNull(contextType, "contextType")); //$NON-NLS-1$
 	}
 
-	@Override
-	public Map<Sequence<Class<?>>, ContextualResolveStrategy<?, ?, Context>> newInstance() {
-		return ServiceClassDiscovery.discoverGenericsMappedImplementations(ContextualResolveStrategy.class, null, null, this.contextType);
-	}
 }
